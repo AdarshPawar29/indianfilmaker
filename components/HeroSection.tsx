fa8d26c7b95e1196e70eb804c9fcfad4f37f873a
its-arun <arun.11601678@lpu.in>
2024-10-16 13:59:06 +0530

    hero section updated

--- components/HeroSection.tsx
+++ components/HeroSection.tsx
@@ -23,6 +23,53 @@ const HeroSection: React.FC = () => {
         />
       </div>
 
+      <div className="overlay get-height">
+        <div className="hanger">
+          <p className="font-neue text-xl">
+            <span>Located </span>
+            <span>in India </span>
+          </p>
+          <Image
+            src="/hanger.svg"
+            alt="Hanger BG"
+            width={300}
+            height={121}
+            priority
+          />
+          <div className="digital-ball">
+            <div className="overlay"></div>
+            <div className="globe">
+              <div className="globe-wrap">
+                <div className="circle"></div>
+                <div className="circle"></div>
+                <div className="circle"></div>
+                <div className="circle-hor"></div>
+                <div className="circle-hor-middle"></div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <div className="container">
+        <div className="absolute text-left left-[10%] md:left-2/3">
+          <div className="absolute -top-full">
+            <Image
+              src="/arrow.svg"
+              alt="Arrow"
+              width={25}
+              height={25}
+              sizes="100vw"
+              priority
+            />
+          </div>
+          <h4 className="font-neue text-white leading-snug">
+            <span className="block">Freelance</span> Cinematographer &
+            Photographer
+          </h4>
+        </div>
+      </div>
+
       <div className="big-name">
         <div className="name-h1">
           <h1 className="font-neue text-white text-xl">
@@ -31,11 +78,6 @@ const HeroSection: React.FC = () => {
             </span>
           </h1>
         </div>
-      </div>
-
-      <div className="absolute top-20 right-10 z-10 text-right">
-        <p className="text-white text-2xl">Cinematographer</p>
-        <p className="text-white text-2xl">Photographer</p>
       </div>
     </header>
   );
