16eba13f8bd3891b2f1d03106e8a74fd34a10255
its-arun <arun.11601678@lpu.in>
2024-10-16 20:40:35 +0530

    fixed now?

--- app/api/send-email/route.ts
+++ app/api/send-email/route.ts
@@ -36,3 +36,17 @@
 //     );
 //   }
 // }
+
+//Mock API
+import type { NextApiRequest, NextApiResponse } from "next";
+
+type ResponseData = {
+  message: string;
+};
+
+export default function handler(
+  req: NextApiRequest,
+  res: NextApiResponse<ResponseData>
+) {
+  res.status(200).json({ message: "Hello from Next.js!" });
+}
